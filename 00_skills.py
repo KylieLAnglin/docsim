56177a6849aed8f8993a884db5c9093f0d2f7314
KylieLAnglin <kylie@anglin.com>
2020-05-20 09:36:30 -0400

    clean skill files

---
+++ 00_skills.py
@@ -0,0 +1,67 @@
+# %%
+import pandas as pd
+
+dir = '/Users/kylie/docsim/'
+skills_path = dir + 'data/coaching_notes/'
+
+# %% Imports
+fall_2017_skills = pd.read_csv(skills_path + 'Fall 2017 Skills.csv')
+fall_2017_skills = fall_2017_skills.rename(columns={'Candidate ID': 'id',
+                                                    'Skill': 'skill'})
+
+spring_2018_skills = pd.read_csv(skills_path + 'Spring 2018 Skills.csv')
+spring_2018_skills = spring_2018_skills.rename(columns={'Candidate ID': 'id',
+                                                        'Skill': 'skill'})
+
+fall_2018_notes = pd.read_csv(skills_path + 'Fall 2018 Coaching Notes.csv')
+fall_2018_notes = fall_2018_notes[['Q1', 'Q16', 'Q6']]
+
+spring_2019_notes = pd.read_csv(skills_path + 'Spring 2019 Coaching Notes.csv')
+spring_2019_notes = spring_2019_notes[['Q37', 'Q38', 'Q31']]
+
+ids_2018_2019 = pd.read_csv(skills_path + 'Linking Roster 2018.csv')
+
+
+# %% Merges and clean
+feedback_skill_labels = {'No coaching conversation, assigned to self reflection': 0,
+                         "Probing for textual evidence": 1,
+                         "Scaffolding students' understanding": 2,
+                         "Providing high-quality descriptive feedback": 3,
+                         "Probing for a warrant": 4}
+
+behavior_skill_labels = {"Timely redirection": 1,
+                         "Specific redirection": 2,
+                         "Succinct redirection": 3,
+                         "Calm redirection": 4}
+
+
+fall_2018_skills = fall_2018_notes.merge(ids_2018_2019, how='inner',
+                                         left_on='Q16', right_on='Email',
+                                         indicator=True)
+fall_2018_skills['Skill'] = fall_2018_skills['Q6'].map(feedback_skill_labels)
+fall_2018_skills = fall_2018_skills[['ID', 'Skill']]
+fall_2018_skills = fall_2018_skills.rename(
+    columns={'ID': 'id', 'Skill': 'skill'})
+
+
+spring_2019_skills = spring_2019_notes.merge(ids_2018_2019, how='inner',
+                                             left_on='Q38', right_on='Email', 
+                                             indicator=True)
+spring_2019_skills['Skill'] = spring_2019_skills['Q31'].map(
+    behavior_skill_labels)
+spring_2019_skills = spring_2019_skills[['ID', 'Skill']]
+spring_2019_skills = spring_2019_skills.rename(
+    columns={'ID': 'id', 'Skill': 'skill'})
+
+
+# %% Exports
+
+for df, file in zip([fall_2017_skills, spring_2018_skills,
+                     fall_2018_skills, spring_2019_skills],
+                    ['fall_2017_skills.csv', 'spring_2018_skills.csv',
+                     'fall_2018_skills.csv', 'spring_2019_skills.csv']):
+
+    df.to_csv(skills_path + file)
+
+
+# %%

8115528f3fde987e4585d2756f416e012682aee5
KylieLAnglin <kylie@anglin.com>
2020-05-20 16:13:14 -0400

    add skill column to clean_text_ideal

--- 01b_clean_text_ideal.py
+++ 01b_clean_text_ideal.py
@@ -6,6 +6,12 @@
 # In[1]:
 
 
+from sklearn import metrics
+from scipy import spatial
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from collections import Counter
+from sklearn.feature_extraction.text import TfidfVectorizer
 import docx
 import os
 import nltk
@@ -13,28 +19,20 @@ import re
 from nltk import sent_tokenize, word_tokenize
 import pandas as pd
 
-from nltk import WordNetLemmatizer 
+from nltk import WordNetLemmatizer
 import operator
 
 import fnmatch
 
 import statistics
-lemmatizer = WordNetLemmatizer() 
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-from collections import Counter
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy import spatial
-from sklearn import metrics
+lemmatizer = WordNetLemmatizer()
 
 
 # In[2]:
+dir = '/Users/kylie/docsim/'
 
-
-ideal_filepath = '/Users/kylieleblancKylie/domino/docsim/data/models/'
-clean_filepath = "/Users/kylieleblancKylie/domino/docsim/data/clean/"
+ideal_filepath = dir + '/data/models/'
+clean_filepath = dir + "/data/clean/"
 
 
 # # Extract Text from Documents
@@ -50,6 +48,7 @@ def make_text_dict(filepath, pattern):
             doc_text[file] = clean_text(filepath + file)
     return doc_text
 
+
 def clean_text(filename):
     doc = docx.Document(filename)
     fullText = []
@@ -60,6 +59,7 @@ def clean_text(filename):
     new_text = ' '.join(fullText)
     return new_text
 
+
 def fix_typos(string):
     new_text = string.replace('00:00:11]', '')
     new_text = new_text.replace('Dave', 'Dev')
@@ -69,7 +69,8 @@ def fix_typos(string):
 # In[4]:
 
 
-ideal_behavior_dict = make_text_dict(ideal_filepath, 'Classroom Management Model*')
+ideal_behavior_dict = make_text_dict(
+    ideal_filepath, 'Classroom Management Model*')
 ideal_behavior_dict
 
 
@@ -83,13 +84,24 @@ ideal_feedback_dict = make_text_dict(ideal_filepath, 'Feedback Model*')
 
 
 def create_df(textdict, scenario):
-    df = pd.DataFrame.from_dict(data = textdict, orient = 'index').reset_index()
-    df = df.rename({'index': 'doc', 0: 'text'}, axis = 'columns')
+    df = pd.DataFrame.from_dict(data=textdict, orient='index').reset_index()
+    df = df.rename({'index': 'doc', 0: 'text'}, axis='columns')
     df['scenario'] = scenario
     df = df.set_index('doc')
     return df
+
+
 ideal_feedback = create_df(ideal_feedback_dict, 'feedback')
-ideal_behavior = create_df(ideal_behavior_dict,'behavior')
+ideal_behavior = create_df(ideal_behavior_dict, 'behavior')
+
+# %% Add skill column
+skill_dict = {' A': '1', ' B': '2', ' C': '3', ' D': '4'}
+
+ideal_feedback['skill'] = ideal_feedback.index.str[14:16]
+ideal_feedback['skill'] = ideal_feedback['skill'].map(skill_dict).astype(int)
+
+ideal_behavior['skill'] = ideal_behavior.index.str[26:28]
+ideal_behavior['skill'] = ideal_behavior['skill'].map(skill_dict).astype(int)
 
 
 # In[7]:
@@ -98,11 +110,4 @@ ideal_behavior = create_df(ideal_behavior_dict,'behavior')
 ideal_corpus_df = ideal_feedback.append(ideal_behavior)
 ideal_corpus_df.sample(5)
 
-
-# # Send to CSV
-
-# In[8]:
-
-
 ideal_corpus_df.to_csv(clean_filepath + 'text_scripts.csv')
-

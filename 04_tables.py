5d75be5be06f3c99e9bcfb28a97c86a38885bc08
KylieLAnglin <kylie@anglin.com>
2020-05-26 17:43:39 -0400

    Revert "refactor 04_tables"
    This reverts commit 6e9958d3ec62e9da1d93fbce4bb9d8873fd183bb.

--- 04_tables.py
+++ 04_tables.py
@@ -37,26 +37,6 @@ results.sample(3)
 techniques = ['', '_stop', '_stop_wgt', '_stem_stop_wgt', '_lsa_wgt_stop']
 columns = [2, 3, 4, 5, 6]
 
-# %%
-spring2018 = results[((results.year == '2017-18') &
-                      (results.semester == 'spring'))]
-print(len(spring2018))
-
-spring2019 = results[((results.year == '2018-19') &
-                      (results.semester == 'spring'))]
-print(len(spring2019))
-
-fall2019 = results[((results.year == '2019-20') &
-                    (results.semester == 'fall'))]
-print(len(fall2019))
-
-fall2017 = results[((results.year == '2017-18') &
-                    (results.semester == 'fall'))]
-print(len(fall2017))
-
-fall2018 = results[((results.year == '2018-19') &
-                    (results.semester == 'fall'))]
-print(len(fall2018))
 
 # In[6]:
 
@@ -71,28 +51,28 @@ row = 3
 # Spring 2018
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(spring2018.script_sim.mean(),
+    value = round(results[((results.year == '2017-18') &
+                           (results.semester == 'spring'))].script_sim.mean(),
                   2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row + 1
 
-
 # Spring 2019
-
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(spring2019.script_sim.mean(),
+    value = round(results[((results.year == '2018-19') &
+                           (results.semester == 'spring'))].script_sim.mean(),
                   2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row + 1
 
 # Fall 2019
-
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(fall2019.script_sim.mean(),
+    value = round(results[((results.year == '2019-20') &
+                           (results.semester == 'fall'))].script_sim.mean(),
                   2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
@@ -100,7 +80,6 @@ row = row + 1
 
 # Feedback
 # Fall 2017
-
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
     value = round(results[((results.year == '2017-18') &
@@ -110,12 +89,11 @@ for tech, col in zip(techniques, columns):
 wb.save(file)
 row = row + 1
 
-
 # Fall 2018
-
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(fall2018.script_sim.mean(), 2)
+    value = round(results[((results.year == '2018-19') &
+                           (results.semester == 'fall'))].script_sim.mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 
@@ -127,26 +105,29 @@ wb = load_workbook(file)
 ws = wb.active
 
 row = 3
+# spring 2017
+for tech, col in zip(techniques, columns):
+    results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
+    value = round(results[((results.year == '2017-18') &
+                           (results.semester == 'spring'))].study_sim_spring2017_18.mean(), 2)
+    ws.cell(row=row, column=col).value = value
+wb.save(file)
+row = row + 1
+
 # spring 2018
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(spring2018.study_sim_spring2017_18.mean(), 2)
+    value = round(results[((results.year == '2018-19') &
+                           (results.semester == 'spring'))].study_sim_spring2017_18.mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row + 1
 
-# spring 2019
+# Spring 2018
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(spring2019.study_sim_spring2017_18.mean(), 2)
-    ws.cell(row=row, column=col).value = value
-wb.save(file)
-row = row + 1
-
-# Fall 2019
-for tech, col in zip(techniques, columns):
-    results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(fall2019.study_sim_spring2017_18.mean(), 2)
+    value = round(results[((results.year == '2019-20') &
+                           (results.semester == 'fall'))].study_sim_spring2017_18.mean(), 2)
     ws.cell(row=5, column=col).value = value
 wb.save(file)
 row = row + 1
@@ -154,7 +135,8 @@ row = row + 1
 # Fall 2017
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(fall2017.study_sim_spring2017_18.mean(), 2)
+    value = round(results[((results.year == '2017-18') &
+                           (results.semester == 'fall'))].study_sim_spring2017_18.mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row + 1
@@ -162,7 +144,8 @@ row = row + 1
 # Fall 2018
 for tech, col in zip(techniques, columns):
     results = pd.read_csv(clean_filepath + 'results' + tech + '.csv')
-    value = round(fall2018.study_sim_spring2017_18.mean(), 2)
+    value = round(results[((results.year == '2018-19') &
+                           (results.semester == 'fall'))].study_sim_spring2017_18.mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 
@@ -182,19 +165,22 @@ cols = [2, 3, 4, 5, 6]
 
 row = 3
 for comp, col in zip(comps, cols):
-    value = round(spring2018[comp].mean(), 2)
+    value = round(results[((results.year == '2017-18') &
+                           (results.semester == 'spring'))][comp].mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row+1
 
 for comp, col in zip(comps, cols):
-    value = round(spring2019[comp].mean(), 2)
+    value = round(results[((results.year == '2018-19') &
+                           (results.semester == 'spring'))][comp].mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row+1
 
 for comp, col in zip(comps, cols):
-    value = round(fall2019[comp].mean(), 2)
+    value = round(results[((results.year == '2019-20') &
+                           (results.semester == 'fall'))][comp].mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row+1
@@ -207,7 +193,8 @@ wb.save(file)
 row = row+1
 
 for comp, col in zip(comps, cols):
-    value = round(fall2017[comp].mean(), 2)
+    value = round(results[((results.year == '2018-19') &
+                           (results.semester == 'fall'))][comp].mean(), 2)
     ws.cell(row=row, column=col).value = value
 wb.save(file)
 row = row+1
@@ -231,11 +218,13 @@ def make_hist(title: str, value_list: list, color: str, range_list: list):
 
 
 make_hist('Figure 1: Fidelity Scores - Behavior Study 1',
-          spring2018.script_sim,
+          results[(results.year == '2017-18') &
+                  (results.semester == 'spring')].script_sim,
           'black', range_list=[0, .5])
 
 make_hist('Figure 2: Fidelity Scores - Behavior Study 2',
-          spring2019.script_sim,
+          results[(results.year == '2018-19') &
+                  (results.semester == 'spring')].script_sim,
           'gray', [0, .5])
 
 
@@ -249,13 +238,15 @@ results = pd.read_csv(clean_filepath + 'results_lsa_wgt_stop.csv')
 bins = np.linspace(0, .5, num=30)
 plt.title('Figure 1: Fidelity Scores for Behavior Study 1 and 2')
 
-study1_values = spring2018.script_sim
+study1_values = results[(results.year == '2017-18') &
+                        (results.semester == 'spring')].script_sim
 plt.hist(study1_values, bins,
          color='darkgray', alpha=.75,
          label='Study 1')
 sns.distplot(study1_values, hist=False, rug=False, color='darkgray')
 
-study2_values = spring2019.script_sim
+study2_values = results[(results.year == '2018-19') &
+                        (results.semester == 'spring')].script_sim
 plt.hist(study2_values,
          bins, color='black', alpha=.5, label='Study 2')
 sns.distplot(study2_values, hist=False, rug=False, color='black')
@@ -267,45 +258,3 @@ plt.ylabel("Number of Documents")
 plt.savefig(table_filepath + 'Figure 1')
 
 plt.show()
-
-# %% Explore by Coach
-bins = np.linspace(0, .5, num=10)
-plt.title('Figure 2: Fidelity Scores for Behavior Study 2 by Coach')
-
-coach1 = results[(results.year == '2018-19') &
-                 (results.semester == 'spring') &
-                 (results.coach == 'Casedy')].script_sim
-
-coach2 = results[(results.year == '2018-19') &
-                 (results.semester == 'spring') &
-                 (results.coach == 'Emily')].script_sim
-
-coach3 = results[(results.year == '2018-19') &
-                 (results.semester == 'spring') &
-                 (results.coach == 'Bryan')].script_sim
-
-coach4 = results[(results.year == '2018-19') &
-                 (results.semester == 'spring') &
-                 (results.coach == 'Arielle')].script_sim
-
-
-sns.distplot(coach1, hist=False, rug=False, color='black',
-             kde_kws={'linestyle': 'solid'}, label='Coach 1')
-
-sns.distplot(coach2, hist=False, rug=False, color='black',
-             kde_kws={'linestyle': 'dotted'}, label='Coach 2')
-
-sns.distplot(coach3, hist=True, rug=False, color='black',
-             kde_kws={'linestyle': 'dashed'}, label='Coach 3')
-
-sns.distplot(coach4, hist=False, rug=False, color='black',
-             kde_kws={'linestyle': 'dashdot'}, label='Coach 4')
-
-plt.legend(loc='upper right')
-plt.xlabel("Fidelity Scores")
-plt.ylabel("Number of Documents")
-# plt.savefig(table_filepath + 'Figure 1')
-
-plt.show()
-
-# %%

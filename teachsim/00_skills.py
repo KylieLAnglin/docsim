9a46dd52baa1a5ef4324eaf50c403811fbfdfc97
KylieLAnglin <kylie@anglin.com>
2020-12-14 11:22:09 -0500

    cut skills not from spring 2019

--- teachsim/00_skills.py
+++ teachsim/00_skills.py
@@ -1,34 +1,13 @@
 # %%
 import pandas as pd
 
-dir = '/Users/kylie/docsim/'
-skills_path = dir + 'data/coaching_notes/'
+from docsim.library import start
+
 
 # %% Imports
-fall_2017_skills = pd.read_csv(skills_path + 'Fall 2017 Skills.csv')
-fall_2017_skills = fall_2017_skills.rename(columns={'Candidate ID': 'id',
-                                                    'Skill': 'skill'})
 
-spring_2018_skills = pd.read_csv(skills_path + 'Spring 2018 Skills.csv')
-spring_2018_skills = spring_2018_skills.rename(columns={'Candidate ID': 'id',
-                                                        'Skill': 'skill'})
-
-fall_2018_notes = pd.read_csv(skills_path + 'Fall 2018 Coaching Notes.csv')
-fall_2018_notes = fall_2018_notes[['Q1', 'Q16', 'Q6']]
-
-spring_2019_notes = pd.read_csv(skills_path + 'Spring 2019 Coaching Notes.csv')
+spring_2019_notes = pd.read_csv(start.raw_filepath + 'Spring_2019/' + 'Spring 2019 Coaching Notes.csv')
 spring_2019_notes = spring_2019_notes[['Q37', 'Q38', 'Q31', 'Q41']]
-
-fall_2019_skills = pd.read_csv(skills_path + 'Fall 2019 TAP Skills.csv')
-
-ids_2018_2019 = pd.read_csv(skills_path + 'Linking Roster 2018.csv')
-
-coach_ids_fall2018 = pd.read_excel(skills_path + 'Linking Roster (Fall 2018).xlsx',
-                                   sheet_name=0)
-coach_ids_fall2018 = coach_ids_fall2018[[
-    'ParticipantID', 'Session3-4_CoachID_New']]
-coach_ids_fall2018 = coach_ids_fall2018.rename(columns={'ParticipantID': 'id',
-                                                        'Session3-4_CoachID_New': 'coach'})
 
 # %% Merges and clean
 feedback_skill_labels = {'No coaching conversation, assigned to self reflection': 0,

e2000298492700fd3ef0fd57eb9656ecfb23c386
KylieLAnglin <kylie@anglin.com>
2020-12-15 10:10:17 -0500

    create text df

--- teachsim/01_clean_text.py
+++ teachsim/01_clean_text.py
@@ -4,27 +4,69 @@ import pandas as pd
 from docsim.library import start
 from docsim.library import clean_text
 
-# %% Import text
+# %% Functions
+def fold_nested_dictionary(input_dict: dict, inner_key: str):
+    """Given an input dictionary with keys mapping to inner dictionaries, extract
+        the value of some common key to the inner dictionary and return the new
+        "folded" dictionary.
+
+    Arguments:
+        input_dict (Dict[Any, Dict[str, Any]]): The input dictionary containing dictionaries as values
+        inner_key (str): The key to use to extract a given value out of the inner dictionaries.
+
+    Returns:
+        A new "folded" dictionary consisting of the original dictionary's keys and the values of the inner_key.
+    """
+    return {k: v[inner_key] for k, v in input_dict.items()}
+
+
+def replace_string_value_as_list(input_dict: dict):
+    """Given an input dictionary, replace string values with list of one string
+
+    Args:
+        input_dict (dict): Dictionary containing string values
+
+    Returns:
+        [type]: Dictionary containing list values of one item each
+    """
+    return {k: [v] for k, v in input_dict.items()}
 
 
 # %% Import text and clean
+
+
 spring2019_filepath = start.raw_filepath + "spring_2019/coaching/"
 
-spring2019_docs = clean_text.import_text(
+raw_text = clean_text.import_text(
     filepath=spring2019_filepath, pattern="*docx", paragraph_tag="[New Speaker] "
 )
 
 speaker_tags_df = pd.read_csv(
-    start.raw_filepath + "spring_2019/Coaching/Spring2019_speaker_tags.csv", header=0
+    spring2019_filepath + "Spring2019_speaker_tags.csv", header=0
 )
 temp_dict = speaker_tags_df.set_index("doc").to_dict(orient="index")
-speaker_tags = {k: v["coach"] for k, v in temp_dict.items()}
+speaker_tags = fold_nested_dictionary(temp_dict, "coach")
 
-spring2019_docs = {
-    k: v.replace(speaker_tags[k], "Coach:") for k, v in spring2019_docs.items()
+
+cleaned_speaker_text = {
+    k: v.replace(speaker_tags[k], "Coach:") for k, v in raw_text.items()
 }
 
+coach_text = {
+    k: clean_text.filter_segments(v, "[New Speaker]", filter_tag="Coach:")
+    for k, v in cleaned_speaker_text.items()
+}
+cleaned_coach_text = {k: re.sub(r"\[(.*?)\]", " ", v) for k, v in coach_text.items()}
 
+
+# combine dictionaries
+big_dict = {k: [raw_text[k]] + [cleaned_coach_text[k]] for k in raw_text.keys()}
+
+text_df = pd.DataFrame.from_dict(
+    big_dict,
+    orient="index",
+    columns=["raw_text", "cleaned_coach_text"],
+)
 # %%
 
 

c934e6d4e33837ce9addead07cb9f52ac12189ac
KylieLAnglin <kylie@anglin.com>
2020-12-15 20:33:31 -0500

    create doc sim matrix on scripts too

--- teachsim/02_create_doc_term_matrices.py
+++ teachsim/02_create_doc_term_matrices.py
@@ -12,63 +12,63 @@ df = df_corpus.append(df_ideal)
 
 # %%
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=False
+    df, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=False
 )
 matrix.to_csv(start.clean_filepath + "matrix.csv")
 
 # %%
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=False, tfidf=True, lemma=False, lsa=False
+    df, "clean_text", remove_stopwords=False, tfidf=True, lemma=False, lsa=False
 )
 matrix.to_csv(start.clean_filepath + "matrix_wgt.csv")
 
 # In[5]:
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=False
+    df, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=False
 )
 matrix.to_csv(start.clean_filepath + "matrix_stop.csv")
 
 # %%
 # # Stop and weight
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=False
+    df, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=False
 )
 matrix.to_csv(start.clean_filepath + "matrix_stop_wgt.csv")
 
 # %% Stem
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=False, tfidf=False, lemma=True, lsa=False
+    df, "clean_text", remove_stopwords=False, tfidf=False, lemma=True, lsa=False
 )
 matrix.to_csv(start.clean_filepath + "matrix_stem.csv")
 
 # %% Stop and Stem
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=True, tfidf=False, lemma=True, lsa=False
+    df, "clean_text", remove_stopwords=True, tfidf=False, lemma=True, lsa=False
 )
 matrix.to_csv(start.clean_filepath + "matrix_stop_stem.csv")
 
 
 # %% # Stop Stem and Weight
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=True, tfidf=True, lemma=True, lsa=False
+    df, "clean_text", remove_stopwords=True, tfidf=True, lemma=True, lsa=False
 )
 matrix.to_csv(start.clean_filepath + "matrix_stop_stem_wgt.csv")
 
 # %% LSA
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=True
+    df, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=True
 )
 matrix.to_csv(start.clean_filepath + "matrix_lsa.csv")
 
 # %% # LSA + Stop
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=True
+    df, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=True
 )
 matrix.to_csv(start.clean_filepath + "matrix_stop_wgt_lsa.csv.csv")
 
 
 # %% LSA, Weighting, Stop Words
 matrix = vectorize.vectorize_text(
-    df_corpus, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=True
+    df, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=True
 )
 matrix.to_csv(start.clean_filepath + "matrix_stop_wgt_lsa.csv")
--- teachsim/03_script_session_study_similarity.py
+++ teachsim/03_script_session_study_similarity.py
@@ -1,46 +1,60 @@
-
-# # Session and Study Similarity
-# * Session similarity for all five studies
-# * Study Similarity
-#     * Fall 2017 to Fall 2018 (Cross-Sample)
-#     * Spring 2018 to Spring 2019 to Fall 2019 (Cross-Sample)
-#     * Fall 2017 to Spring 2018 (Cross-Setting)
-#     * Fall 2018 to Spring 2019 (Cross-Setting)
-# * Each pre-processing technique receives its own df
-
 # %%
 import pandas as pd
-from scipy import spatial
-from openpyxl import load_workbook
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.decomposition import TruncatedSVD
-from sklearn.preprocessing import Normalizer
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 from docsim.library import start
-
-clean_filepath = start.clean_filepath
-table_filepath = start.table_filepath
-# %%
-
-transcript_df = pd.read_csv(clean_filepath + 'text_transcripts.csv')
-transcript_df = transcript_df.set_index('doc')
-transcript_df.sample(5)
-
-# %%
-script_df = pd.read_csv(clean_filepath + 'text_scripts.csv')
-script_df = script_df.set_index('doc')
-script_df.sample(5)
+from docsim.library import analyze
 
 # %%
 
+transcript_df = pd.read_csv(start.clean_filepath + "text_transcripts.csv")
+script_df = pd.read_csv(start.clean_filepath + "text_scripts.csv")
 
+matrix = pd.read_csv(start.clean_filepath + "matrix_stop_wgt_lsa.csv")
+
+# %%
+# Script similarity
+# loop through index of transcripts, calc sim to all relevant scripts. Keep the highest score
+# what are the relevant scripts for spring2019
+
+
+def max_sim(transcript_matrix, script_matrix):
+    sims_df = pd.DataFrame({"doc": transcript_matrix.index.tolist()}).set_index("doc")
+    for script in script_matrix.index:
+        sims = distance_to_doc(transcript_matrix, script_matrix.loc[script])
+        sims_df[script] = sims
+
+    sims_df["script_sim"] = sims_df[script_matrix.index.tolist()].max(axis=1)
+
+    return sims_df[["script_sim"]]
+
+
+script_results = pd.DataFrame()
+for scenario in ["feedback", "behavior"]:
+    for skill in [1, 2, 3, 4]:
+        sim_df = script_similarity(
+            matrix_transcripts_file="matrix_transcripts.csv",
+            limited_transcript_df=transcript_df[
+                ((transcript_df.scenario == scenario) & (transcript_df.skill == skill))
+            ],
+            matrix_scripts_file="matrix_scripts.csv",
+            limited_script_df=script_df[
+                (script_df.scenario == scenario) & (script_df.skill == skill)
+            ],
+        )
+        script_results = script_results.append(sim_df)
+
+
+results = results.merge(script_results, left_index=True, right_index=True)
+results.to_csv((clean_filepath + "results.csv"))
+
+
+# %%
 def distance_to_mean(matrix_main, matrix_comparison):
     sims = []
     for maindoc in matrix_main.index:
-        matrix_comp = matrix_comparison[~matrix_comparison.index.isin(
-            [maindoc])]  # exclude self
+        matrix_comp = matrix_comparison[
+            ~matrix_comparison.index.isin([maindoc])
+        ]  # exclude self
         centroid = np.mean(matrix_comp)
         sim = 1 - spatial.distance.cosine(matrix_main.loc[maindoc], centroid)
         sims.append(sim)
@@ -62,14 +76,15 @@ def pairwise_distance(matrix_main, matrix_comparison):
     ave_sims = []
     for maindoc in matrix_main.index:
         sims = []
-        matrix_comp = matrix_comparison[~matrix_comparison.index.isin(
-            [maindoc])]  # exclude self
+        matrix_comp = matrix_comparison[
+            ~matrix_comparison.index.isin([maindoc])
+        ]  # exclude self
         for compdoc in matrix_comp.index:
-            sim = 1 - \
-                spatial.distance.cosine(
-                    matrix_main.loc[maindoc], matrix_comp.loc[compdoc])
+            sim = 1 - spatial.distance.cosine(
+                matrix_main.loc[maindoc], matrix_comp.loc[compdoc]
+            )
             sims.append(sim)
-        ave_sim = sum(sims)/len(sims)
+        ave_sim = sum(sims) / len(sims)
         ave_sims.append(ave_sim)
     return ave_sims
 
@@ -80,134 +95,147 @@ def session_similarity(matrix_transcripts_file, text_df):
     Returns new df with variable.
     """
     matrix_transcripts = pd.read_csv(clean_filepath + matrix_transcripts_file)
-    matrix_transcripts = matrix_transcripts.set_index('doc')
-    results = text_df[(text_df.year == '2017-18') &
-                      (text_df.semester == 'fall')]
+    matrix_transcripts = matrix_transcripts.set_index("doc")
+    results = text_df[(text_df.year == "2017-18") & (text_df.semester == "fall")]
     matrix = matrix_transcripts[matrix_transcripts.index.isin(results.index)]
     sims = pairwise_distance(matrix, matrix)
-    results['session_sim'] = sims
-    semesters = ['spring', 'fall', 'spring', 'fall']
-    years = ['2017-18', '2018-19', '2018-19', '2019-20']
+    results["session_sim"] = sims
+    semesters = ["spring", "fall", "spring", "fall"]
+    years = ["2017-18", "2018-19", "2018-19", "2019-20"]
     for semester, year in zip(semesters, years):
         df = text_df[(text_df.year == year) & (text_df.semester == semester)]
         matrix = matrix_transcripts[matrix_transcripts.index.isin(df.index)]
         sims = pairwise_distance(matrix, matrix)
-        df['session_sim'] = sims
+        df["session_sim"] = sims
         results = results.append(df)
     return results
 
 
 def study_similarity(matrix_transcripts_file, text_df):
     """
-    Calculates the distance of every document to the mean of every study. 
+    Calculates the distance of every document to the mean of every study.
     Returns new df with variable for every study distance.
     """
-    semesters = ['fall', 'spring', 'fall', 'spring', 'fall']
-    years = ['2017-18', '2017-18', '2018-19', '2018-19', '2019-20']
+    semesters = ["fall", "spring", "fall", "spring", "fall"]
+    years = ["2017-18", "2017-18", "2018-19", "2018-19", "2019-20"]
 
     matrix_transcripts = pd.read_csv(clean_filepath + matrix_transcripts_file)
-    matrix_transcripts = matrix_transcripts.set_index('doc')
+    matrix_transcripts = matrix_transcripts.set_index("doc")
 
     for semester, year in zip(semesters, years):
         matrix = matrix_transcripts
 
         comp = text_df[(text_df.year == year) & (text_df.semester == semester)]
-        matrix_comp = matrix_transcripts[matrix_transcripts.index.isin(
-            comp.index)]
+        matrix_comp = matrix_transcripts[matrix_transcripts.index.isin(comp.index)]
 
         sims = pairwise_distance(matrix, matrix_comp)
-        results['study_sim_' + semester + year[0:4] + '_' + year[5:7]] = sims
+        results["study_sim_" + semester + year[0:4] + "_" + year[5:7]] = sims
     return results
 
 
-def script_similarity(matrix_transcripts_file, limited_transcript_df,
-                      matrix_scripts_file, limited_script_df):
+def script_similarity(
+    matrix_transcripts_file,
+    limited_transcript_df,
+    matrix_scripts_file,
+    limited_script_df,
+):
     # limit transcripts
-    transcript_matrix = import_limited_matrices(matrix_transcripts_file,
-                                                limited_transcript_df)
+    transcript_matrix = import_limited_matrices(
+        matrix_transcripts_file, limited_transcript_df
+    )
 
     # limit scripts
-    script_matrix = import_limited_matrices(matrix_scripts_file,
-                                            limited_script_df)
+    script_matrix = import_limited_matrices(matrix_scripts_file, limited_script_df)
 
-    sims_df = max_sim(transcript_matrix=transcript_matrix,
-                      script_matrix=script_matrix)
+    sims_df = max_sim(transcript_matrix=transcript_matrix, script_matrix=script_matrix)
 
     return sims_df
 
 
 def max_sim(transcript_matrix, script_matrix):
-    sims_df = pd.DataFrame(
-        {'doc': transcript_matrix.index.tolist()}).set_index('doc')
+    sims_df = pd.DataFrame({"doc": transcript_matrix.index.tolist()}).set_index("doc")
     for script in script_matrix.index:
         sims = distance_to_doc(transcript_matrix, script_matrix.loc[script])
         sims_df[script] = sims
 
-    sims_df['script_sim'] = sims_df[script_matrix.index.tolist()].max(axis=1)
+    sims_df["script_sim"] = sims_df[script_matrix.index.tolist()].max(axis=1)
 
-    return sims_df[['script_sim']]
+    return sims_df[["script_sim"]]
 
 
-def import_limited_matrices(matrix_file: str,
-                            limited_text_df: pd.DataFrame):
+def import_limited_matrices(matrix_file: str, limited_text_df: pd.DataFrame):
     matrix = pd.read_csv(clean_filepath + matrix_file)
-    matrix = matrix.set_index('doc')
+    matrix = matrix.set_index("doc")
 
     # limit to scenario scripts
-    limited_matrix = matrix[matrix.index.isin(
-        limited_text_df.index)]
+    limited_matrix = matrix[matrix.index.isin(limited_text_df.index)]
 
     return limited_matrix
 
 
 # %%
 # No pre-processing
-results = session_similarity(
-    'matrix_transcripts.csv', transcript_df)  # session sim col
-results = study_similarity('matrix_transcripts.csv',
-                           transcript_df)  # creates study sim cols
+results = session_similarity("matrix_transcripts.csv", transcript_df)  # session sim col
+results = study_similarity(
+    "matrix_transcripts.csv", transcript_df
+)  # creates study sim cols
 
 script_results = pd.DataFrame()
-for scenario in ['feedback', 'behavior']:
+for scenario in ["feedback", "behavior"]:
     for skill in [1, 2, 3, 4]:
         sim_df = script_similarity(
-            matrix_transcripts_file='matrix_transcripts.csv',
-            limited_transcript_df=transcript_df[(
-                (transcript_df.scenario == scenario) &
-                (transcript_df.skill == skill))],
-            matrix_scripts_file='matrix_scripts.csv',
-            limited_script_df=script_df[(script_df.scenario == scenario) &
-                                        (script_df.skill == skill)])
+            matrix_transcripts_file="matrix_transcripts.csv",
+            limited_transcript_df=transcript_df[
+                ((transcript_df.scenario == scenario) & (transcript_df.skill == skill))
+            ],
+            matrix_scripts_file="matrix_scripts.csv",
+            limited_script_df=script_df[
+                (script_df.scenario == scenario) & (script_df.skill == skill)
+            ],
+        )
         script_results = script_results.append(sim_df)
 
 
 results = results.merge(script_results, left_index=True, right_index=True)
-results.to_csv((clean_filepath + 'results.csv'))
+results.to_csv((clean_filepath + "results.csv"))
 
 # %%
-techniques = ['_stop', '_stop_wgt', '_stem', '_stem_stop',
-              '_stem_stop_wgt', '_lsa', '_lsa_stop', '_lsa_wgt_stop']
+techniques = [
+    "_stop",
+    "_stop_wgt",
+    "_stem",
+    "_stem_stop",
+    "_stem_stop_wgt",
+    "_lsa",
+    "_lsa_stop",
+    "_lsa_wgt_stop",
+]
 for tech in techniques:
-    matrix_transcripts_file = 'matrix_transcripts' + tech + '.csv'
-    matrix_scripts_file = 'matrix_scripts' + tech + '.csv'
+    matrix_transcripts_file = "matrix_transcripts" + tech + ".csv"
+    matrix_scripts_file = "matrix_scripts" + tech + ".csv"
     results = session_similarity(matrix_transcripts_file, transcript_df)
     results = study_similarity(matrix_transcripts_file, transcript_df)
 
     script_results = pd.DataFrame()
-    for scenario in ['feedback', 'behavior']:
+    for scenario in ["feedback", "behavior"]:
         for skill in [1, 2, 3, 4]:
             sim_df = script_similarity(
                 matrix_transcripts_file=matrix_transcripts_file,
-                limited_transcript_df=transcript_df[(
-                    (transcript_df.scenario == scenario) &
-                    (transcript_df.skill == skill))],
+                limited_transcript_df=transcript_df[
+                    (
+                        (transcript_df.scenario == scenario)
+                        & (transcript_df.skill == skill)
+                    )
+                ],
                 matrix_scripts_file=matrix_scripts_file,
-                limited_script_df=script_df[(script_df.scenario == scenario) &
-                                            (script_df.skill == skill)])
+                limited_script_df=script_df[
+                    (script_df.scenario == scenario) & (script_df.skill == skill)
+                ],
+            )
             script_results = script_results.append(sim_df)
 
     results = results.merge(script_results, left_index=True, right_index=True)
-    results.to_csv((clean_filepath + 'results' + tech + '.csv'))
+    results.to_csv((clean_filepath + "results" + tech + ".csv"))
 
 
 # %%

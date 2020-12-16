ba148883da84f87685aac5266532fd3f85c8527d
KylieLAnglin <kylie@anglin.com>
2020-12-15 21:03:35 -0500

    re-clean ideal text with new naming

--- teachsim/01b_clean_text_ideal.py
+++ teachsim/01b_clean_text_ideal.py
@@ -6,19 +6,18 @@ from docsim.library import clean_text
 ideal_behavior_dict = clean_text.import_text(
     start.raw_filepath + "models/", pattern="Classroom Management*.docx"
 )
-ideal_behavior_df = pd.DataFrame.from_dict(ideal_behavior_dict, orient="index")
-ideal_behavior_df["scenario"] = "behavior"
-ideal_behavior_df["study"] = "model"
+df = pd.DataFrame.from_dict(ideal_behavior_dict, orient="index")
+df = df.rename(columns={0: "clean_text"})
 
+df["scenario"] = "behavior"
+df["study"] = "model"
 
-# %% Add skill column
-skill_dict = {" A": "1", " B": "2", " C": "3", " D": "4"}
-ideal_behavior_df["skill"] = ideal_behavior_df.index.str[26:28]
-ideal_behavior_df["skill"] = ideal_behavior_df["skill"].map(skill_dict).astype(int)
+df["skill"] = [int(list(filter(str.isdigit, doc))[0]) for doc in df.index]
+df = df.reset_index().rename(columns={"index": "doc"})
+df["id"] = [doc[-6:-5] for doc in df.doc]
+df["id"] = df.scenario + df.skill.astype(str) + df.id
 
-ideal_behavior_df = ideal_behavior_df.rename(columns={0: "clean_text"})
-ideal_behavior_df = ideal_behavior_df.reset_index().rename(columns={"index": "doc"})
-
+df = df.set_index("id")
 # %%
 
-ideal_behavior_df.to_csv(start.clean_filepath + "text_scripts.csv", index=False)
+df.to_csv(start.clean_filepath + "text_scripts.csv", index=False)

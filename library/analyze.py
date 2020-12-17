78415d3c7be5f88feb88f85c027c67ac649e0a53
KylieLAnglin <kylie@anglin.com>
2020-12-17 09:35:19 -0500

    add max sim of rows

--- library/analyze.py
+++ library/analyze.py
@@ -17,13 +17,49 @@ def cosine_similarity_row(matrix: pd.DataFrame, index1, index2):
     return 1 - spatial.distance.cosine(matrix.loc[index1], matrix.loc[index2])
 
 
-def max_sim_of_rows(matrix: pd.DataFrame, main_index, comp_indices: list):
-    try:
-        return max(
-            [cosine_similarity_row(matrix, main_index, comp) for comp in comp_indices]
-        )
-    except:
-        return None
-
-
 # TODO: fix so that if there are no matches?
+def max_sim_of_rows(matrix: pd.DataFrame, main_index, comp_indices: list):
+    """[summary]
+
+    Args:
+        matrix (pd.DataFrame): doc-term matrix
+        main_index ([type]): index of row to calculate similarity to other rows
+        comp_indices (list): indices of comparison rows
+
+    Returns:
+        [type]: [description]
+    """
+    return max(
+        [cosine_similarity_row(matrix, main_index, comp) for comp in comp_indices]
+    )
+
+
+def row_is_peer(df: pd.DataFrame, main_row, col_to_match: str):
+    """[summary]
+
+    Args:
+        df (pd.Dataframe): dataframe containing rows and peers
+        main_row (index): row with value to match
+        col_to_match (str): column to match
+
+    Returns:
+        [type]: list of booleans for whether row is peer
+
+    Rows are peers to themselves.
+    """
+
+    return list(df[col_to_match] == df.loc[main_row][col_to_match])
+
+
+def filter_df_with_dict(df: pd.DataFrame, filter_rules: dict):
+    """Filters rows of a df using col, value rules in dict
+
+    Args:
+        df (pd.DataFrame): df to filter
+        filter_rules (dict): dictionary mapping columns to needed column values
+
+    Returns:
+        [type]: [description]
+    """
+
+    return df.loc[(df[list(filter_rules)] == pd.Series(filter_rules)).all(axis=1)]
--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -16,3 +16,25 @@ def test_max_sim_of_rows():
 
     result = analyze.max_sim_of_rows(test, "test1", ["test2", "test3"])
     assert result == 1
+
+
+def test_row_is_peer():
+
+    test = pd.DataFrame({"col1": [1, 2, 2]})
+    result = analyze.row_is_peer(test, 1, "col1")
+
+    assert result[2] == True
+
+
+def test_filter_df_with_dict():
+    test = pd.DataFrame({"col1": [1, 2, 2, 1], "col2": [2, 3, 4, 2]})
+    test_rule = {"col1": 1, "col2": 2}
+    result = analyze.filter_df_with_dict(test, test_rule)
+
+    assert list(result.index) == [0, 3]
+
+    test = pd.DataFrame({"col1": [1, 2, 2, 1], "col2": [2, 3, 4, 2]})
+    test_rule = {}
+    result = analyze.filter_df_with_dict(test, test_rule)
+
+    assert list(result.index) == [0, 1, 2, 3]

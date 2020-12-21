112a4649b58768b6eb506ae4826dd5173f78d701
KylieLAnglin <kal3nh@virginia.edu>
2020-12-21 17:07:47 -0600

    rename variables in skills

--- library/start.py
+++ library/start.py
@@ -1,5 +1,7 @@
-data_dir = "/Users/kylie/Dropbox/docsim/"
-code_dir = "/Users/kylie/docsim/docsim/"
+main_dir = "/Users/kylieanglin/"
+
+data_dir = main_dir + "Dropbox/docsim/"
+code_dir = main_dir + "docsim/docsim/"
 
 raw_filepath = data_dir + "data/"
 clean_filepath = data_dir + "data/clean/"
--- teachsim/00_skills.py
+++ teachsim/00_skills.py
@@ -4,30 +4,32 @@ import pandas as pd
 from docsim.library import start
 from docsim.library import qualtrics
 
-# %% Import data
-spring2019_path = start.raw_filepath + "Spring_2019/" + "Spring 2019 Coaching Notes.csv"
-spring2019_notes = pd.read_csv(spring2019_path)
+# %% Fall 2017
 
-ids_2018_2019 = pd.read_csv(
-    start.raw_filepath + "coaching_notes/" + "Linking Roster 2018.csv"
-)
-
-# %% Clean coaching notes
-
-spring2019_notes = qualtrics.select_valid_rows(
-    spring2019_notes, keep_previews=True, min_duration=0
-)
-spring2019_skills = spring2019_notes[["Q37", "Q38", "Q31", "Q41"]].rename(
+# %% Spring 2019
+#
+# Import and clean coaching notes
+path = start.raw_filepath + "Spring_2019/" + "Spring 2019 Coaching Notes.csv"
+notes = pd.read_csv(path)
+notes = qualtrics.select_valid_rows(notes, keep_previews=True, min_duration=0)
+skills = notes[["Q37", "Q38", "Q31", "Q41"]].rename(
     columns={"Q37": "name", "Q38": "email", "Q41": "coach", "Q31": "skill_name"}
 )
 
-# %% Link coach notes to participant ids
-spring2019_skills = spring2019_skills.merge(
-    ids_2018_2019[["ID", "Email"]], how="left", left_on="email", right_on="Email"
+# Import linking doc
+path = start.raw_filepath + "coaching_notes/" + "Linking Roster 2018.csv"
+ids = pd.read_csv(path)
+
+# Merge coach notes to participant ids
+skills = skills.merge(
+    ids[["ID", "Email"]], how="left", left_on="email", right_on="Email"
 )  # missing 14/99. These people may have transcripts.
 # So these are qualtrics surveys by a coach that do not link to the \
 # linking doc with student IDs. Don't know yet how many transcripts don't \
 # link to a coach
+
+spring2019 = skills
+
 
 # %% Label skills
 behavior_skill_labels = {

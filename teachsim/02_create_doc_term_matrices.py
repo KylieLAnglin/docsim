a354cbcc38991fe17a1cc84c9d5e13359b927a29
KylieLAnglin <kal3nh@virginia.edu>
2020-12-24 09:37:18 -0600

    add text columns to matrices

--- teachsim/02_create_doc_term_matrices.py
+++ teachsim/02_create_doc_term_matrices.py
@@ -20,6 +20,16 @@ df = df_corpus.append(df_ideal)
 matrix = process_text.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=False
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=False,
+        lemma=False,
+    )
+    for text in df.clean_text
+]
 
 matrix.to_csv(start.clean_filepath + "matrix.csv")
 
@@ -27,12 +37,32 @@ matrix.to_csv(start.clean_filepath + "matrix.csv")
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=True, lemma=False, lsa=False
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=False,
+        lemma=False,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_wgt.csv")
 
-# In[5]:
+# %%
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=False
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=True,
+        lemma=False,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stop.csv")
 
 # %%
@@ -40,18 +70,48 @@ matrix.to_csv(start.clean_filepath + "matrix_stop.csv")
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=False
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=True,
+        lemma=False,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stop_wgt.csv")
 
 # %% Stem
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=False, lemma=True, lsa=False
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=False,
+        lemma=True,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stem.csv")
 
 # %% Stop and Stem
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=False, lemma=True, lsa=False
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=True,
+        lemma=True,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stop_stem.csv")
 
 
@@ -59,18 +119,48 @@ matrix.to_csv(start.clean_filepath + "matrix_stop_stem.csv")
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=True, lsa=False
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=True,
+        lemma=True,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stop_stem_wgt.csv")
 
 # %% LSA
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=True
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=False,
+        lemma=False,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_lsa.csv")
 
 # %% # LSA + Stop
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=True
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=True,
+        lemma=False,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stop_lsa.csv")
 
 
@@ -78,10 +168,30 @@ matrix.to_csv(start.clean_filepath + "matrix_stop_lsa.csv")
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=True
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=True,
+        lemma=False,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stop_wgt_lsa.csv")
 
 # %%
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=True, lsa=True
 )
+matrix["text"] = [
+    process_text.process_text(
+        text=text,
+        lower_case=True,
+        remove_punct=True,
+        remove_stopwords=True,
+        lemma=True,
+    )
+    for text in df.clean_text
+]
 matrix.to_csv(start.clean_filepath + "matrix_stop_stem_wgt_lsa.csv")

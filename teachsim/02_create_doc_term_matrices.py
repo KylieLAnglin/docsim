0474cfd5accc0954453ec9fd133420a3ca621607
KylieLAnglin <kal3nh@virginia.edu>
2020-12-24 09:51:40 -0600

    add prefix to docterm matrix

--- teachsim/02_create_doc_term_matrices.py
+++ teachsim/02_create_doc_term_matrices.py
@@ -19,7 +19,7 @@ df = df_corpus.append(df_ideal)
 
 matrix = process_text.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=False
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -36,7 +36,7 @@ matrix.to_csv(start.clean_filepath + "matrix.csv")
 # %%
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=True, lemma=False, lsa=False
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -52,7 +52,7 @@ matrix.to_csv(start.clean_filepath + "matrix_wgt.csv")
 # %%
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=False
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -69,7 +69,7 @@ matrix.to_csv(start.clean_filepath + "matrix_stop.csv")
 # # Stop and weight
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=False
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -85,7 +85,7 @@ matrix.to_csv(start.clean_filepath + "matrix_stop_wgt.csv")
 # %% Stem
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=False, lemma=True, lsa=False
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -101,7 +101,7 @@ matrix.to_csv(start.clean_filepath + "matrix_stem.csv")
 # %% Stop and Stem
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=False, lemma=True, lsa=False
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -118,7 +118,7 @@ matrix.to_csv(start.clean_filepath + "matrix_stop_stem.csv")
 # %% # Stop Stem and Weight
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=True, lsa=False
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -134,7 +134,7 @@ matrix.to_csv(start.clean_filepath + "matrix_stop_stem_wgt.csv")
 # %% LSA
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=False, tfidf=False, lemma=False, lsa=True
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -150,7 +150,7 @@ matrix.to_csv(start.clean_filepath + "matrix_lsa.csv")
 # %% # LSA + Stop
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=False, lemma=False, lsa=True
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -167,7 +167,7 @@ matrix.to_csv(start.clean_filepath + "matrix_stop_lsa.csv")
 # %% LSA, Weighting, Stop Words
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=False, lsa=True
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,
@@ -183,7 +183,7 @@ matrix.to_csv(start.clean_filepath + "matrix_stop_wgt_lsa.csv")
 # %%
 matrix = vectorize.vectorize_text(
     df, "clean_text", remove_stopwords=True, tfidf=True, lemma=True, lsa=True
-)
+).add_prefix("term_")
 matrix["text"] = [
     process_text.process_text(
         text=text,

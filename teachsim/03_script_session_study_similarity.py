4c191cc325a45cde7823a81c0c1c4cdb664d2689
KylieLAnglin <kal3nh@virginia.edu>
2020-12-25 15:05:16 -0600

    add other processing techniques

--- teachsim/03_script_session_study_similarity.py
+++ teachsim/03_script_session_study_similarity.py
@@ -6,48 +6,108 @@ from docsim.library import analyze
 
 # %%
 
-transcript_df = pd.read_csv(start.clean_filepath + "text_transcripts.csv").set_index(
-    "id"
+transcript_df = pd.read_csv(start.clean_filepath + "text_transcripts.csv")
+transcript_df["id"] = transcript_df.id.astype(str)
+transcript_df = transcript_df.set_index(["study", "id"])
+
+script_df = pd.read_csv(start.clean_filepath + "text_scripts.csv").set_index(
+    ["study", "id"]
 )
-script_df = pd.read_csv(start.clean_filepath + "text_scripts.csv").set_index("id")
-df = transcript_df.append(script_df)
+text_df = transcript_df.append(script_df)
 
-df = df[["study", "year", "semester", "scenario", "coach", "skill"]]
-df.index = df.index.map(str)
+text_df = text_df[["year", "semester", "scenario", "coach", "skill"]]
 
-matrix = pd.read_csv(start.clean_filepath + "matrix_stop_wgt_lsa.csv").set_index("id")
-matrix = matrix.add_prefix("term_")
-matrix.index = matrix.index.map(str)
+# %% Create results
+def create_results(merged_df: pd.DataFrame):
 
-df = df.merge(matrix, how="left", left_index=True, right_index=True)
+    new_df = merged_df.copy()
+
+    new_df["peers"] = analyze.row_matches_in_lists(new_df, col_to_match="skill")
+
+    doc_term_matrix = new_df[list(new_df.filter(regex=("term")))]
+
+    new_df["script_sim"] = [
+        analyze.max_sim_of_rows(
+            matrix=doc_term_matrix,
+            main_index=i,
+            comp_indices=list(
+                set(new_df.xs("model", drop_level=False).index)
+                & set(new_df.loc[i].peers)
+            ),
+        )
+        for i in new_df.index
+    ]
+
+    results = new_df[["year", "semester", "scenario", "skill", "coach", "script_sim"]]
+
+    return results
+
+
+# %% No Pre-Processing
+matrix = pd.read_csv(start.clean_filepath + "matrix.csv")
+matrix["id"] = matrix.id.astype(str)
+matrix = matrix.set_index(["study", "id"])
+
+df = text_df.merge(matrix, how="left", left_index=True, right_index=True)
+
+results = create_results(merged_df=df)
+results.to_csv(start.clean_filepath + "results.csv")
+
+# %%
+matrix = pd.read_csv(start.clean_filepath + "matrix_stop.csv")
+matrix["id"] = matrix.id.astype(str)
+matrix = matrix.set_index(["study", "id"])
+
+df = text_df.merge(matrix, how="left", left_index=True, right_index=True)
+
+results = create_results(merged_df=df)
+results.to_csv(start.clean_filepath + "results_stop.csv")
+
+# %%
+# %%
+matrix = pd.read_csv(start.clean_filepath + "matrix_stop_wgt.csv")
+matrix["id"] = matrix.id.astype(str)
+matrix = matrix.set_index(["study", "id"])
+
+df = text_df.merge(matrix, how="left", left_index=True, right_index=True)
+
+results = create_results(merged_df=df)
+results.to_csv(start.clean_filepath + "results_stop_wgt.csv")
+# %%
+matrix = pd.read_csv(start.clean_filepath + "matrix_stop_wgt.csv")
+matrix["id"] = matrix.id.astype(str)
+matrix = matrix.set_index(["study", "id"])
+
+df = text_df.merge(matrix, how="left", left_index=True, right_index=True)
+
+results = create_results(merged_df=df)
+results.to_csv(start.clean_filepath + "results_stop_wgt.csv")
 
 # %%
 
+matrix = pd.read_csv(start.clean_filepath + "matrix_stop_stem_wgt.csv")
+matrix["id"] = matrix.id.astype(str)
+matrix = matrix.set_index(["study", "id"])
 
+df = text_df.merge(matrix, how="left", left_index=True, right_index=True)
+
+results = create_results(merged_df=df)
+results.to_csv(start.clean_filepath + "results_stop_stem_wgt.csv")
 # %%
-doc_term_matrix = df[list(df.filter(regex=("term")))]
+matrix = pd.read_csv(start.clean_filepath + "matrix_stop_wgt_lsa.csv")
+matrix["id"] = matrix.id.astype(str)
+matrix = matrix.set_index(["study", "id"])
 
-df["peers"] = analyze.row_matches_in_lists(df, col_to_match="skill")
-study_groups = df.groupby(["study", "scenario"])
+df = text_df.merge(matrix, how="left", left_index=True, right_index=True)
 
+results = create_results(merged_df=df)
+results.to_csv(start.clean_filepath + "results_stop_wgt_lsa.csv")
+#%%
+matrix = pd.read_csv(start.clean_filepath + "matrix_stop_stem_wgt_lsa.csv")
+matrix["id"] = matrix.id.astype(str)
+matrix = matrix.set_index(["study", "id"])
 
-new_df = df.loc[study_groups.groups["spring2019", "behavior"]]
-new_df["script_sim"] = [
-    analyze.max_sim_of_rows(
-        matrix=doc_term_matrix,
-        main_index=i,
-        comp_indices=list(
-            set(study_groups.groups["model", "behavior"]) & set(df.loc[i].peers)
-        ),
-    )
-    for i in study_groups.groups["spring2019", "behavior"]
-]
+df = text_df.merge(matrix, how="left", left_index=True, right_index=True)
 
-new_df = new_df[
-    ["study", "year", "semester", "scenario", "skill", "coach", "script_sim"]
-]
-# %%
-
-new_df.to_csv(start.clean_filepath + "results_spring2019.csv")
-
-# %%
+results = create_results(merged_df=df)
+results.to_csv(start.clean_filepath + "results_stop_stem_wgt_lsa.csv")

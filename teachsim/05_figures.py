8a60060b63010633ce674e4988c012629c7cfb9f
KylieLAnglin <kal3nh@virginia.edu>
2020-12-26 08:15:17 -0600

    clean figures but coach boxplot broken

--- teachsim/05_figures.py
+++ teachsim/05_figures.py
@@ -1,69 +1,85 @@
+# %%
 
-# In[1]:
-
-import pandas as pd
-from openpyxl import load_workbook
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import seaborn as sns
-from library import start
+from openpyxl import load_workbook
 
-# In[2]:
+from docsim.library import start
 
-clean_filepath = start.clean_filepath
-table_filepath = start.table_filepath
-
-# In[3]:
-
-docs = pd.read_csv(clean_filepath + 'text_transcripts.csv')
-docs = docs.set_index('doc')
-docs.sample(5)
 
 # %%
-results = pd.read_csv(clean_filepath + 'results_lsa_wgt_stop.csv')
+results = pd.read_csv(start.clean_filepath + "results_stop_wgt_lsa.csv").set_index(
+    "study", "id"
+)
 
 sns.set_style("white")
 
 # %% Fidelity Results
-###
-# Fidelity Results
-###
-study1_values = results[(results.year == '2017-18') &
-                        (results.semester == 'spring')].script_sim
-study2_values = results[(results.year == '2018-19') &
-                        (results.semester == 'spring')].script_sim
-study3_values = results[(results.year == '2019-20') &
-                        (results.semester == 'fall')].script_sim
-study4_values = results[(results.year == '2017-18') &
-                        (results.semester == 'fall')].script_sim
-study5_values = results[(results.year == '2018-19') &
-                        (results.semester == 'fall')].script_sim
+
+study1_values = results.loc["spring2018"].script_sim
+study2_values = results.loc["spring2019"].script_sim
+study3_values = results.loc["fall2019TAP"].script_sim
+study4_values = results.loc["fall2017"].script_sim
+study5_values = results.loc["fall2018"].script_sim
+
 
 # %% Figure 1 Fidelity Score Distributions
 
 fig = plt.figure(figsize=(10, 10))
 ax = plt.axes()
 
-#ax.set_title('Figure 1: Fidelity Score Distributions', fontsize=15)
+# ax.set_title('Figure 1: Fidelity Score Distributions', fontsize=15)
 
-bins = np.linspace(0, .5, num=10)
+bins = np.linspace(0, 0.5, num=10)
 
-sns.distplot(study1_values, hist=False, rug=False, color='0.01',
-             kde_kws={'linestyle': 'solid'}, label='Behavior Study 1')
+sns.distplot(
+    study1_values,
+    hist=False,
+    rug=False,
+    color="0.01",
+    kde_kws={"linestyle": "solid"},
+    label="Behavior Study 1",
+)
 
-sns.distplot(study2_values, hist=False, rug=False, color='black',
-             kde_kws={'linestyle': 'dashed'}, label='Behavior Study 2')
+sns.distplot(
+    study2_values,
+    hist=False,
+    rug=False,
+    color="black",
+    kde_kws={"linestyle": "dashed"},
+    label="Behavior Study 2",
+)
 
-sns.distplot(study3_values, hist=False, rug=False, color='black',
-             kde_kws={'linestyle': 'dotted'}, label='Behavior Study 3')
+sns.distplot(
+    study3_values,
+    hist=False,
+    rug=False,
+    color="black",
+    kde_kws={"linestyle": "dotted"},
+    label="Behavior Study 3",
+)
 
-sns.distplot(study4_values, hist=False, rug=False, color='lightgray',
-             kde_kws={'linestyle': 'solid'}, label='Feedback Study 1')
+sns.distplot(
+    study4_values,
+    hist=False,
+    rug=False,
+    color="lightgray",
+    kde_kws={"linestyle": "solid"},
+    label="Feedback Study 1",
+)
 
-sns.distplot(study5_values, hist=False, rug=False, color='lightgray',
-             kde_kws={'linestyle': 'dashed'}, label='Feedback Study 2')
+sns.distplot(
+    study5_values,
+    hist=False,
+    rug=False,
+    color="lightgray",
+    kde_kws={"linestyle": "dashed"},
+    label="Feedback Study 2",
+)
 
-ax.legend(loc='upper right')
+ax.legend(loc="upper right")
 ax.set_xlabel("Adherence Scores")
 ax.set_ylabel("Kernel Density")
 # notes = "Notes: Fidelity scores are estimated by calculating the " \
@@ -71,33 +87,45 @@ ax.set_ylabel("Kernel Density")
 #     "A higher score indicates higher fidelity to the script."
 # fig.text(.1, .05, notes, ha='left', wrap=True)
 
-fig.savefig(table_filepath + 'Figure 1 Fidelity Score Distributions',
-            bbox='tight', pad_inches=0.05)
+fig.savefig(
+    start.table_filepath + "Figure 1 Fidelity Score Distributions",
+    bbox="tight",
+    pad_inches=0.05,
+)
 
 
-for study in [study1_values, study2_values, study3_values,
-              study4_values, study5_values]:
+for study in [
+    study1_values,
+    study2_values,
+    study3_values,
+    study4_values,
+    study5_values,
+]:
     print(study.std())
 
 # %% Figure 2
 fig = plt.figure(figsize=(10, 10))
 ax = plt.axes()
 
-#ax.set_title('Figure 2: Fidelity Scores for Feedback Study 2 by Coach',
+# ax.set_title('Figure 2: Fidelity Scores for Feedback Study 2 by Coach',
 #             fontsize=15)
 
-coach_code = {'Casedy': 'C', 'Sarah': 'A', 'Alex': 'D', 'Arielle': 'B'}
-results['coach_code'] = results['coach'].map(coach_code)
+coach_code = {"Casedy": "C", "Sarah": "A", "Alex": "D", "Arielle": "B"}
+results["coach_code"] = results["coach"].map(coach_code)
 
 
-sns.boxplot(x='coach_code', y='script_sim',
-            data=results[(results.year == '2018-19') &
-                         (results.semester == 'fall')],
-            color='white')
-sns.swarmplot(x='coach_code', y='script_sim',
-              data=results[(results.year == '2018-19') &
-                           (results.semester == 'fall')],
-              color="black")
+sns.boxplot(
+    x="coach_code",
+    y="script_sim",
+    data=results.loc["fall2018"],
+    color="white",
+)
+sns.swarmplot(
+    x="coach_code",
+    y="script_sim",
+    data=results.loc["fall2018"],
+    color="black",
+)
 
 ax.set_xlabel("Coach")
 ax.set_ylabel("Adherence Scores")
@@ -113,14 +141,11 @@ ax.set_ylabel("Adherence Scores")
 # fig.text(.1, .025, notes, ha='left', wrap=True)
 
 
-fig.savefig(table_filepath +
-            'Figure 2 Fidelity Scores for Feedback Study 2 by Coach')
+fig.savefig(table_filepath + "Figure 2 Fidelity Scores for Feedback Study 2 by Coach")
 plt.show()
 
-for coach in ['A', 'B', 'C', 'D']:
-    print(results[(results.semester == 'fall') &
-                  (results.year == '2018-19') &
-                  (results.coach_code == coach)].script_sim.median())
+for coach in ["A", "B", "C", "D"]:
+    print(results.loc["fall2018"][(results.coach_code == coach)].script_sim.median())
 
 # %% Figure 3: Panel of Histograms
 
@@ -132,46 +157,45 @@ ax3 = axs[0, 2]
 ax4 = axs[1, 0]
 ax5 = axs[1, 1]
 
-bins = np.linspace(0, .75, num=30)
+bins = np.linspace(0, 0.75, num=30)
 
-#fig.suptitle('Figure 3: Fidelity Scores  with Unusual Transcripts Highlighted',
- #            fontsize=15)
+# fig.suptitle('Figure 3: Fidelity Scores  with Unusual Transcripts Highlighted',
+#            fontsize=15)
 
-ax1.hist(study1_values, bins,
-         color='darkgray', alpha=.75)
-ax1.set_xlabel('Behavior Study 1')
+ax1.hist(study1_values, bins, color="darkgray", alpha=0.75)
+ax1.set_xlabel("Behavior Study 1")
 
-ax2.hist(study2_values, bins,
-         color='darkgray', alpha=.75)
-ax2.set_xlabel('Behavior Study 2')
-ax2.hist(study2_values.where(study2_values < .2), bins,
-         color='black')
+ax2.hist(study2_values, bins, color="darkgray", alpha=0.75)
+ax2.set_xlabel("Behavior Study 2")
+ax2.hist(
+    study2_values.where((study2_values < 0.18) | (study2_values > 0.37)),
+    bins,
+    color="black",
+)
 
-ax3.hist(study3_values, bins,
-         color='darkgray', alpha=.75)
-ax3.set_xlabel('Behavior Study 3')
-ax3.hist(study3_values.where(study3_values > 0.36), bins,
-         color='black')
+ax3.hist(study3_values, bins, color="darkgray", alpha=0.75)
+ax3.set_xlabel("Behavior Study 3")
+ax3.hist(study3_values.where(study3_values < 0.20), bins, color="black")
 
-ax4.hist(study4_values, bins,
-         color='darkgray', alpha=1)
-ax4.set_xlabel('Feedback Study 1')
-ax4.hist(study4_values.where((study4_values < .09) | (study4_values > 0.31)),
-         bins, color='black')
+ax4.hist(study4_values, bins, color="darkgray", alpha=1)
+ax4.set_xlabel("Feedback Study 1")
+ax4.hist(
+    study4_values.where((study4_values > 0.35)),
+    bins,
+    color="black",
+)
 
-ax5.hist(study5_values, bins,
-         color='darkgray', alpha=1)
-ax5.set_xlabel('Feedback Study 2')
-ax5.hist(study5_values.where((study5_values > 0.61)), bins,
-         color='black')
+ax5.hist(study5_values, bins, color="darkgray", alpha=1)
+ax5.set_xlabel("Feedback Study 2")
+ax5.hist(study5_values.where((study5_values > 0.61)), bins, color="black")
 
 for ax in [ax1, ax2, ax3, ax4, ax5]:
     ax.set_ylim(0, 20)
 
 fig.delaxes(axs[1, 2])
 
-fig.text(0.5, 0.04, 'Adherence Scores', ha='center')
-fig.text(0.04, 0.5, 'Number of Transcripts', va='center', rotation='vertical')
+fig.text(0.5, 0.04, "Adherence Scores", ha="center")
+fig.text(0.04, 0.5, "Number of Transcripts", va="center", rotation="vertical")
 
 # notes = "Notes: Fidelity scores are estimated by calculating the " \
 #     "similarity between each transcript and an ideal script. " \
@@ -181,7 +205,7 @@ fig.text(0.04, 0.5, 'Number of Transcripts', va='center', rotation='vertical')
 #     " flagged for manual observation."
 # fig.text(.1, .025, notes, ha='left')
 
-plt.savefig(table_filepath + 'Figure 3: Fidelity Panels', dpi=200)
+plt.savefig(start.table_filepath + "Figure 3: Fidelity Panels", dpi=200)
 
 
 # %% Figure 2b
@@ -189,22 +213,25 @@ plt.savefig(table_filepath + 'Figure 3: Fidelity Panels', dpi=200)
 plt.figure(figsize=(10, 10))
 sns.set_style("white")
 
-coach_code = {'Casedy': 1, 'Emily': 2, 'Bryan': 3, 'Arielle': 4}
-results['coach_code'] = results['coach'].map(coach_code)
+coach_code = {"Casedy": 1, "Emily": 2, "Bryan": 3, "Arielle": 4}
+results["coach_code"] = results["coach"].map(coach_code)
 
 
-sns.boxplot(x='coach_code', y='script_sim',
-            data=results[(results.year == '2018-19') &
-                         (results.semester == 'spring')],
-            color='white')
-sns.swarmplot(x='coach_code', y='script_sim',
-              data=results[(results.year == '2018-19') &
-                           (results.semester == 'spring')],
-              color="black")
+sns.boxplot(
+    x="coach_code",
+    y="script_sim",
+    data=results[(results.year == "2018-19") & (results.semester == "spring")],
+    color="white",
+)
+sns.swarmplot(
+    x="coach_code",
+    y="script_sim",
+    data=results[(results.year == "2018-19") & (results.semester == "spring")],
+    color="black",
+)
 plt.xlabel("Coach")
 plt.ylabel("Adherence Scores")
-plt.savefig(table_filepath +
-            'Figure 2b Fidelity Scores for Behavior Study 2 by Coach')
+plt.savefig(table_filepath + "Figure 2b Fidelity Scores for Behavior Study 2 by Coach")
 
 plt.show()
 

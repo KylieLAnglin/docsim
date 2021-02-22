d22104c71eade138834753a599536c86b33718c5
KylieLAnglin <kylie@anglin.com>
2021-02-22 14:47:23 -0500

    clean clean_gold_standard (no more manual fixes)

--- teachsim/clean_gold_standard.py
+++ teachsim/clean_gold_standard.py
@@ -24,6 +24,24 @@ gold = gold.rename(
         "SC3": "quality",
     }
 )
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("-updated", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("_updated", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace(" (updated)", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("_Transcript", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace(" Transcript", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("Scored", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("Score", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace(".docx", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace(".doc", "")
+
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace(
+    "C", "c"
+)  # make sure already dropped c words
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("c_", "")
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("c", "")
+
+
+gold["qualtrics_filename"] = gold.qualtrics_filename.str.replace("-", "_")
 
 
 # %%
@@ -42,6 +60,11 @@ transcripts = transcripts[
     ]
 ]
 transcripts["filename"] = transcripts.filename.str.replace(".docx", "")
+transcripts["filename"] = transcripts.filename.str.replace("_Transcript", "")
+transcripts["filename"] = transcripts.filename.str.replace("C", "c")
+transcripts["filename"] = transcripts.filename.str.replace("c", "")
+transcripts["filename"] = transcripts.filename.str.replace("-", "_")
+
 # %%
 linking = transcripts.merge(
     gold,
@@ -59,17 +82,11 @@ linking[["study", "id", "filename", "qualtrics_filename", "_merge"]].to_csv(
 
 # %%
 
-corrected_linking = pd.read_excel(
-    start.RAW_FILEPATH + "qualtrics_linking.xls", engine="xlrd"
-)
-# %%
-cleaned_gold = gold[["qualtrics_filename", "fidelity", "quality"]].merge(
-    corrected_linking[["filename", "qualtrics_filename"]],
-    left_on="qualtrics_filename",
+cleaned_gold = transcripts.merge(
+    gold[["qualtrics_filename", "fidelity", "quality"]],
+    how="left",
+    left_on="filename",
     right_on="qualtrics_filename",
-)
-cleaned_gold = cleaned_gold.merge(
-    transcripts, how="left", left_on="filename", right_on="filename"
 )
 
 # %%
@@ -89,11 +106,15 @@ cleaned_gold = cleaned_gold[
         "quality",
     ]
 ]
+
+cleaned_gold = cleaned_gold[cleaned_gold.scenario == "behavior"]
 # %% Random hold-out sample
 subset = cleaned_gold.sample(60, random_state=7)
 cleaned_gold["training"] = np.where(cleaned_gold.index.isin(subset.index), 1, 0)
 
 # %%
-cleaned_gold.to_csv(start.RAW_FILEPATH + "/validation/" + "human_codes.csv")
+cleaned_gold.to_csv(
+    start.RAW_FILEPATH + "/validation/" + "human_codes.csv", index=False
+)
 
 # %%

c12ffdd463362af6523a37173928ecff58bd0942
KylieLAnglin <kylie@anglin.com>
2021-07-27 08:08:05 -0500

    create plain text scripts

--- explore/create plain text files.py
+++ explore/create plain text files.py
@@ -14,4 +14,18 @@ for n in list(range(len(transcripts))):
     f.write(transcripts[n])
 
 
+# %% Create plain text models
+
+df = pd.read_csv("/Users/kylie/Dropbox/Active/docsim/data/clean/text_scripts.csv")
+scripts = list(df.clean_text)
+file_names = [filename for filename in df.filename]
+OUTPUT = "/Users/kylie/Dropbox/Active/docsim/data/plain text scripts/"
+
+
+for n in list(range(len(scripts))):
+    print(n)
+    f = open(OUTPUT + file_names[n] + ".txt", "w+")
+    f.write(scripts[n])
+
+
 # %%
